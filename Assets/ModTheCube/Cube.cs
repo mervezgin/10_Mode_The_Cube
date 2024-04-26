7e8ea65ad1f0db2588dcee8602e122a4b2f03c86
mervezgin <mervezgin@gmail.com>
2024-04-26 11:01:32 +0300

    some changes

--- Assets/ModTheCube/Cube.cs
+++ Assets/ModTheCube/Cube.cs
@@ -13,11 +13,11 @@ public class Cube : MonoBehaviour
         
         Material material = Renderer.material;
         
-        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+        material.color = new Color(3.5f, 1.0f, 3.3f, 0.4f);
     }
     
     void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+        transform.Rotate(15.0f * Time.deltaTime, 0.0f, 0.0f);
     }
 }
